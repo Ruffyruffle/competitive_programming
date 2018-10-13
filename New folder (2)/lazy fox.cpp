501efa9ef61918bc1513deed88f6649000345636
Steven Ung <stevenung1234567890@gmail.com>
2018-10-13 18:52:23 -0400

    lazy

---
+++ New folder (2)/lazy fox.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct line{
+    int x1,x2,y1,y2,dis;
+    line(int ax1, int ax2, int ay1, int ay2, int adis){
+        x1 = ax1; x2 = ax2; y1 = ay1; y2 = ay2; dis = adis;
+    }
+};
+
+int main(){
+
+    vector<pair<int,int> > po;
+    int n,a,b;
+    for(int i = 0; i<n; i++){
+        cin>>a>>b; po.push_back({a,b});
+    }
+}
